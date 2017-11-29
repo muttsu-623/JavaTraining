63f4028fbba394c859748e331c5fb6e48f5b6c0e
muttsu-623 <mutsumi1996@gmail.com>
2017-11-29 17:19:25 +0900

    5章リスト5_2
    メソッドの呼び出し方法をhelloメソッドをmainメソッドで呼ぶということから学んだ.

---
+++ src/JavaTraining5/JavaTraining5_2/Main.java
@@ -0,0 +1,12 @@
+package JavaTraining5.JavaTraining5_2;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("メソッドを呼び出します。");
+        hello();
+        System.out.println("メソッドの呼び出しが終わりました。");
+    }
+    public static void hello() {
+        System.out.println("Hello World");
+    }
+}
