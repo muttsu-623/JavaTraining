3fb37accaec818a20d3a09f8dd3835d599840b2e
muttsu-623 <mutsumi1996@gmail.com>
2017-11-29 17:35:12 +0900

    6章CalcLogicクラスを作成
    Calcクラスのmainメソッドで呼ばれるtasu, hikuというメソッドを記述した.

---
+++ src/JavaTraining6/JavaTraining6_1/CalcLogic.java
@@ -0,0 +1,13 @@
+package JavaTraining6.JavaTraining6_1;
+
+public class CalcLogic {
+    // 足し算を行う関数
+    public static int tasu(int a, int b) {
+        return a + b;
+    }
+
+    // 引き算を行う関数
+    public static int hiku(int a, int b) {
+        return a - b;
+    }
+}
