7ce332fcf56d9a979d903f03fdd996798540b513
muttsu-623 <mutsumi1996@gmail.com>
2017-11-29 09:51:36 +0000

    6章トレーニング6_1(second_sessionクラスの作成)
    second_sessionクラスを作成し, mainクラスの後半部分のメソッドを移行.

--- src/JavaTraining6/JavaTraining6_1/calcapp/main/Calc.java
+++ src/JavaTraining6/JavaTraining6_1/calcapp/main/Calc.java
@@ -6,8 +6,8 @@ public class Calc {
         int b = 5;
 
         // 他のクラスのメソッドを呼ぶときは, クラス名.メソッド名() と記述する.
-        int total = CalcLogic.tasu(a, b);
-        int delta = CalcLogic.hiku(a, b);
+        int total = JavaTraining6.JavaTraining6_1.calcapp.logic.CalcLogic.tasu(a, b);
+        int delta = JavaTraining6.JavaTraining6_1.calcapp.logic.CalcLogic.hiku(a, b);
 
         System.out.println("10 + 5 = "+ total);
         System.out.println("10 - 5 = "+ delta);
---
+++ src/JavaTraining6/Training6_1/second_session.java
@@ -0,0 +1,12 @@
+package JavaTraining6.Training6_1;
+
+public class second_session {
+    public static void callDeae(){
+        System.out.println("えぇい、こしゃくな。くせ者だ!であえい!");
+    }
+    public static void showMondokoro() throws Exception {
+        System.out.println("飛車さん、角さん。もういいでしょう。");
+        System.out.println("この紋所が目にはいらぬか! ");
+        first_session.doTogame(); // もう一度、とがめる
+    }
+}
