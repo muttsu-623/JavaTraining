6bc31edf56dc9b378a187913b7139e8939b6f089
muttsu-623 <mutsumi1996@gmail.com>
2017-11-29 17:34:18 +0900

    6章Calcクラスを作成
    計算を行うためのプログラムとして, mainメソッドを記述した.

---
+++ src/JavaTraining6/JavaTraining6_1/Calc.java
@@ -0,0 +1,15 @@
+package JavaTraining6.JavaTraining6_1;
+
+public class Calc {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+
+        // 他のクラスのメソッドを呼ぶときは, クラス名.メソッド名() と記述する.
+        int total = CalcLogic.tasu(a, b);
+        int delta = CalcLogic.hiku(a, b);
+        
+        System.out.println("10 + 5 = "+ total);
+        System.out.println("10 - 5 = "+ delta);
+    }
+}
