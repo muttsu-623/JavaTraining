5ad786e5829faea996dde9c7e65c8040d7f70142
muttsu-623 <mutsumi1996@gmail.com>
2017-11-29 17:23:31 +0900

    5章リスト5_7
    返り値について学んだ.
    メソッド名の前に返り値の型を記述する.

---
+++ src/JavaTraining5/JavaTraining5_7/Main.java
@@ -0,0 +1,12 @@
+package JavaTraining5.JavaTraining5_7;
+
+public class Main {
+    public static int add(int x, int y) {
+        int ans = x + y;
+        return ans;
+    }
+    public static void main(String[] args) {
+        int ans = add(100, 200);
+        System.out.println("100 + 200 = " + ans);
+    }
+}
