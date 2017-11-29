7c21026442125e0a90622ef4758fca5937438a49
muttsu-623 <mutsumi1996@gmail.com>
2017-11-29 17:25:56 +0900

    5章リスト5_11
    オーバーロードに関して学んだ.
    この場合は, 引数の数の違いによりメソッドを認識する.

---
+++ src/JavaTraining5/JavaTraining5_11/Main.java
@@ -0,0 +1,13 @@
+package JavaTraining5.JavaTraining5_11;
+
+public class Main {
+    public static int add(int x, int y) {
+        return x + y;
+    }
+    public static int add(int x, int y, int z) {
+        return x + y + z;
+    }
+    public static void main(String[] args) {
+        System.out.println("10 + 20 = " + add(10, 20));
+    }
+}
