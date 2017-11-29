352c919e197297769b2b2ee8bc3b64aab0dc6139
muttsu-623 <mutsumi1996@gmail.com>
2017-11-29 09:50:36 +0000

    6章トレーニング6_1(first_sessionクラスの作成)

---
+++ src/JavaTraining6/Training6_1/first_session.java
@@ -0,0 +1,9 @@
+package JavaTraining6.Training6_1;
+
+public class first_session {
+    public static void doWarusa() {
+        System.out.println("きなこでござる。食えませんがの。");
+    }
+    public static void doTogame() {
+        System.out.println("この老いぼれの目はごまかせませんぞ。"); }
+}
