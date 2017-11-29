ed424244142dab4ba9bcbaba919820feff73db1d
muttsu-623 <mutsumi1996@gmail.com>
2017-11-29 17:24:16 +0900

    5章リスト5_9
    returnの後に記述を行なった場合, コンパイルエラーが起こり出力できない例を確認した.

---
+++ src/JavaTraining5/JavaTraining5_9/Main.java
@@ -0,0 +1,13 @@
+package JavaTraining5.JavaTraining5_9;
+
+public class Main {
+    public static int add(int x, int y) {
+        int ans = x + y;
+        return ans;
+//        int z = 10;
+    }
+    public static void main(String[] args) {
+        int ans = add(100, 200);
+        System.out.println("100 + 200 = " + ans);
+    }
+}
